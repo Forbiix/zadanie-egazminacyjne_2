37d13da02bf87249ec97c2abb4033ea3c78863d3
Kedarini <Forbix@wp.pl>
2024-04-25 09:34:52 +0200

    sty 2024 i  sty 2023
    2024 gotwy, dokończ combobox w 2023

--- zadanie/sty2023.xaml.cs
+++ zadanie/sty2023.xaml.cs
@@ -30,5 +30,90 @@ namespace zadanie
             this.Close();
 
         }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AcceptOnClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("zatwierdzono dane");
+
+
+        }
+
+        public static char[] random_letters(int legth, bool number, bool b_letter, bool special)
+        {
+            Random rnd = new Random();
+            char[] specials = { '!', '@', '#', '$', '%', '^', '&', '*' };
+            char[] letters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'w', 'q', 'y', 'z' };
+            char[] b_letters = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'W', 'Q', 'Y', 'Z' };
+            char[] numbers = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' };
+            char[] password = new char[legth];
+
+            for (int i = 0; i < legth; i++)
+            {
+                password[i] = letters[rnd.Next(letters.Length)];
+            }
+            if (number == true)
+            {
+                password[0] = numbers[rnd.Next(numbers.Length)];
+            }
+            if (special == true)
+            {
+                if (legth > 1)
+                {
+                    password[1] = specials[rnd.Next(specials.Length)];
+                }
+
+            }
+            if (b_letter == true)
+            {
+                if (legth > 2)
+                {
+                    password[2] = b_letters[rnd.Next(b_letters.Length)];
+                }
+            }
+
+            return password;
+        }
+
+        private void zatwierdz(object sender, RoutedEventArgs e)
+        {
+            bool b_letters = false;
+            bool numbers = false;
+            bool specials = false;
+            int password_length = int.Parse(passwordlength.Text);
+            if (c_numbers.IsChecked == true)
+            {
+                numbers = true;
+            }
+            if (c_letters.IsChecked == true)
+            {
+                b_letters = true;
+            }
+            if (c_special.IsChecked == true)
+            {
+                specials = true;
+            }
+            char[] generated_password = random_letters(password_length, numbers, b_letters, specials);
+            string password = "";
+            for (int i = 0; i < generated_password.Length; i++)
+            {
+                password += generated_password[i].ToString();
+            }
+            MessageBox.Show($"Twoje hasło to: {password}");
+        }
     }
 }
--- zadanie/sty2024.xaml.cs
+++ zadanie/sty2024.xaml.cs
@@ -30,5 +30,48 @@ namespace zadanie
             this.Close();
 
         }
+
+        private void numer_LostFocus(object sender, RoutedEventArgs e)
+        {
+
+            string photo_numer = "111";
+            if(numer.Text =="111" || numer.Text =="222" || numer.Text == "333")
+            {
+                face.Visibility = Visibility.Visible;
+                train.Visibility = Visibility.Visible;
+                photo_numer = numer.Text;
+            }
+            else
+            {
+                face.Visibility = Visibility.Collapsed;
+                train.Visibility = Visibility.Collapsed;
+                MessageBox.Show("Niepoprawny numer");
+            }
+            face.Source = new BitmapImage(new Uri($"C:/Users/student/Documents/3ip/git/zadanie-egazminacyjne_2/zadanie/zdjecia/{photo_numer}.jpg"));
+            train.Source = new BitmapImage(new Uri($"C:\\Users\\student\\Documents\\3ip\\git\\zadanie-egazminacyjne_2\\zadanie\\zdjecia\\{photo_numer}-pocaig.png"));
+
+        
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string IMIE = imie.Text;
+            string NAZWISKO = nazwisko.Text;
+            string KOLOR = "";
+            if (rd1.IsChecked == true)
+            {
+                KOLOR = "niebieskie";
+            }
+            if (rd2.IsChecked == true)
+            {
+                KOLOR = "zielone";
+            }
+            if (rd3.IsChecked == true)
+            {
+                KOLOR = "piwne";
+            }
+            MessageBox.Show($"Oto {IMIE} {NAZWISKO}. Jego oczy są {KOLOR}");
+
+        }
     }
 }
